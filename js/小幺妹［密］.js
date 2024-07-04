0cbf8d53a5053229940200b22fc6d8f4ab344fab
xiaokopo1 <160040348+xiaokopo1@users.noreply.github.com>
2024-07-04 18:53:03 +0800

    Update 小幺妹［密］.js

--- js/小幺妹［密］.js
+++ js/小幺妹［密］.js
@@ -1,1 +1,28 @@
-dmFyIHJ1bGUgPSB7CiAg57G75Z6LOiAn5b2x6KeGJywKICB0aXRsZTogJ+Wwj+W5uuWmue+8u+Wvhu+8vScsCiAgaG9zdDogJ2h0dHBzOi8vYWdxeHlhb21laXNwLmJ1enonLAogIHVybDogJy92aWRlby90eXBlL2Z5Y2xhc3MvZnlwYWdlLmh0bWwnLAogIHNlYXJjaFVybDogJy92aWRlby9sb29rdXAvKiovTC9meXBhZ2UuaHRtbCcsCiAgc2VhcmNoYWJsZTogMiwKICBxdWlja1NlYXJjaDogMCwKICBmaWx0ZXJhYmxlOiAxLAogIGhlYWRlcnM6IHsKICAgICdVc2VyLUFnZW50JzogJ01PQklMRV9VQScsCiAgfSwKICB0aW1lb3V0OiA1MDAwLAogIGNsYXNzX3BhcnNlOiAnLnJvdyAuY29sLTM6bHQoNDIpO2EmJnRpdGxlO2EmJmhyZWY7LiovKC4qPykuaHRtbCcsIAogIGNhdGVfZXhjbHVkZTon6KeG6aKR5LiJ5Yy6fOinhumikeS6jOWMunzop4bpopHlm5vljLp854m56Imy5LiT5Yy6JywKICBwbGF5X3BhcnNlOiB0cnVlLAogIGxhenk6ICJqczppbnB1dCA9IHtwYXJzZTogMSwgdXJsOiBpbnB1dCwganM6ICcnfSIsCiAgZG91YmxlOiB0cnVlLAogIOaOqOiNkDogJy5yb3cgLmNvbC1zbS02Oy5jb2wtc20tNjtoMiYmVGV4dDtpbWcmJnNyYzsucG9zaXRpb24tYWJzb2x1dGUmJlRleHQ7YSYmaHJlZicsCiAg5LiA57qnOiAnLmNvbC1zbS02O2gyJiZUZXh0O2ltZyYmc3JjOy50ZXh0LWRhcmsmJlRleHQ7YSYmaHJlZjsuc21hbGwmJlRleHQnLAogIOS6jOe6pzogewogICAgdGl0bGU6ICdoMSYmVGV4dCcsCiAgICBpbWc6ICdpbWcmJnNyYycsCiAgICB0YWJzOiAnJywKICAgIGxpc3RzOiAnLnRhYmxlLXN0cmlwZWQ6ZXEoI2lkKSYmYScsCiAgfSwKICDmkJzntKI6ICcqJywKfQ==
+var rule = {
+  类型: '影视',
+  title: '小幺妹［密］',
+  host: 'https://agqxyaomeisp.buzz',
+  url: '/video/type/fyclass/fypage.html',
+  searchUrl: '/video/lookup/**/L/fypage.html',
+  searchable: 2,
+  quickSearch: 0,
+  filterable: 1,
+  headers: {
+    'User-Agent': 'MOBILE_UA',
+  },
+  timeout: 5000,
+  class_parse: '.row .col-3:lt(42);a&&title;a&&href;.*/(.*?).html', 
+  cate_exclude:'视频三区|视频二区|视频四区|特色专区',
+  play_parse: true,
+  lazy: "js:input = {parse: 1, url: input, js: ''}",
+  double: true,
+  推荐: '.row .col-sm-6;.col-sm-6;h2&&Text;img&&src;.position-absolute&&Text;a&&href',
+  一级: '.col-sm-6;h2&&Text;img&&src;.text-dark&&Text;a&&href;.small&&Text',
+  二级: {
+    title: 'h1&&Text',
+    img: 'img&&src',
+    tabs: '',
+    lists: '.table-striped:eq(#id)&&a',
+  },
+  搜索: '*',
+}
